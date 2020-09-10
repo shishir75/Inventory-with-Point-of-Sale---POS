de86799fe0e9ba810d748db6d11b09ddf67ee67d
shishir75 <shishir.srdr16@gmail.com>
2020-09-11 00:33:00 +0600

    JWT Middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware( 'auth:api', ['except' => ['login', 'signup']] );
+        $this->middleware( 'JWT', ['except' => ['login', 'signup']] );
     }
 
     /**
