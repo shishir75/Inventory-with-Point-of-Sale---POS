0fcc2c9cd2ae5696bf961a05731615d87af63ba1
shishir75 <shishir.srdr16@gmail.com>
2020-09-10 16:20:12 +0600

    Implement JWT in Project

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group( ['middleware' => 'api', 'prefix' => 'auth'], function ( $router ) {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::post( 'login', 'AuthController@login' );
+    Route::post( 'logout', 'AuthController@logout' );
+    Route::post( 'refresh', 'AuthController@refresh' );
+    Route::post( 'me', 'AuthController@me' );
+
+} );
